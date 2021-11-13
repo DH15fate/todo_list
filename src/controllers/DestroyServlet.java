44ffe00965dcb269137c6dfd45e7192732726525
syosf <syosf@192.168.10.24>
2021-11-13 21:24:59 +0900

    削除機能追加

---
+++ src/controllers/DestroyServlet.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Todo;
+import utils.DBUtil;
+/**
+ * Servlet implementation class DestroyServlet
+ */
+@WebServlet("/destroy")
+public class DestroyServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DestroyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            //セッションスコープからTodoのIDを取得し、該当のIDのメッセージ１件のみをDBから取得
+            Todo m = em.find(Todo.class,(Integer)(request.getSession().getAttribute("todo_id")));
+
+            em.getTransaction().begin();
+            em.remove(m);
+            em.getTransaction().commit();
+            em.close();
+
+            //セッションスコープ上の不要になったデータを削除
+            request.getSession().removeAttribute("todo_id");
+
+            //indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+
+        }
+    }
+
+}
